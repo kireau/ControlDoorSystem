3b7866a9f8d2336b7ba5a29d85b28113962c66e6
Kain762 <Kain762@mail.ru>
2022-10-15 15:42:43 +0700

    Добавлен клиент (страница логина)

---
+++ client/components/mainTable.vue
@@ -0,0 +1,12 @@
+<template>
+  <v-container>
+    545454545
+  </v-container>
+</template>
+
+<script>
+  export default {
+    name: 'MainTable'
+
+  }
+</script>
---
+++ client/pages/main.vue
@@ -0,0 +1,17 @@
+<template>
+  <v-container fluid>
+    <main-table></main-table>
+  </v-container>
+</template>
+
+<script>
+import mainTable from "../components/mainTable.vue"
+
+  export default {
+    name: 'MainPage',
+    components: {
+      mainTable,
+    },
+
+  }
+</script>
