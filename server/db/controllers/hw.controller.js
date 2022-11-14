d32f63969425f3750ff8ffe0b3cfd909f624cb6d
kireau <37921752+kireau@users.noreply.github.com>
2022-11-14 16:53:05 +0700

    Update hw.controller.js

--- server/db/controllers/hw.controller.js
+++ server/db/controllers/hw.controller.js
@@ -11,7 +11,11 @@ class HWController {
         // )
         // console.log(keysID.rows)
 
-        const keys = [id_door, '2a', '3', '4', '5', '6', '7', '8', '9']
+        const keys = {
+            "DoorID" : id_door,
+            "Keys" : ["1b729b35a56065", "1b739b35a56061", "2b729b35a56061", "1b529b35a53061", "1a729b35a56061", "1b729b35a56061"]  
+        }
+        
         // for (const el of keysID.rows) {
         //     keys.push( await userController.getUserChip(el.id_user))
         // }
