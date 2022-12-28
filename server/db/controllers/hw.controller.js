facbc8fffd9d1770d2d5cc91fa129af0dbae4160
kireau <37921752+kireau@users.noreply.github.com>
2022-12-28 12:16:05 +0700

    get

--- server/db/controllers/hw.controller.js
+++ server/db/controllers/hw.controller.js
@@ -2,6 +2,9 @@ const db = require('../postgres')
 const userController = require('../controllers/users.controller')
 
 class HWController {
+    
+
+
     // отправка списка ключей для двери
     async getKeyList(req, res) {
         const id_door = req.params.id
@@ -15,21 +18,29 @@ class HWController {
         const keys = {
             "DoorID" : id_door,
             "Keys" : [
-            "1b729b35a56065",
             "1b739b35a56061",
             "2b729b35a56061",
             "1b529b35a53061",
             "1a729b35a56061",
             "1b729b35a56061"
-            ]  
+            ] 
         }
+
+        
         
         // for (const el of keysID.rows) {
         //     keys.push( await userController.getUserChip(el.id_user))
         // }
   
-    res.json(keys)
-        
+    res.json(keys)   
+    }
+
+    // Запись последнего активного ключа из двери
+    async writeLastKey(req, res) {
+        const {id, lastKey} = req.body
+
+        console.log(id + lastKey)
+        res.json(id + lastKey)
     }
 }
 
--- server/db/router/hw.router.js
+++ server/db/router/hw.router.js
@@ -2,6 +2,8 @@ const Router = require('express')
 const router = new Router()
 const hwController = require('../controllers/hw.controller')
 
+
 router.get('/:id', hwController.getKeyList) // отправить список ключей
+router.post('/giveKey', hwController.writeLastKey) // прием последнего ключа двери
 
 module.exports = router
