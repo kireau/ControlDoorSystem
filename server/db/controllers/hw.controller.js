000c3572d950b15bff207e4f9bdcc9df186fd62d
kireau <37921752+kireau@users.noreply.github.com>
2023-01-25 13:25:24 +0700

    ss
    ss

--- server/db/controllers/hw.controller.js
+++ server/db/controllers/hw.controller.js
@@ -1,10 +1,8 @@
 const db = require('../postgres')
 const userController = require('../controllers/users.controller')
+const axios = require('axios')
 
 class HWController {
-    
-
-
     // отправка списка ключей для двери
     async getKeyList(req, res) {
         const id_door = req.params.id
@@ -13,33 +11,47 @@ class HWController {
         //     [id_door]
         // )
         // console.log(keysID.rows)
-
-        //
-        const keys = {
-            "Keys" : [
+        // перебор массива и получение ключей по ID юзера
+        const keys = [
             "1b739b35a56061",
             "2b729b35a56061",
             "1b529b35a53061",
             "1a729b35a56061",
             "1b729b35a56061"
-            ] 
-        }
-
-        
-        
+        ]
         // for (const el of keysID.rows) {
         //     keys.push( await userController.getUserChip(el.id_user))
         // }
   
-    res.json(keys)   
+    res.json(keys)
     }
-
-    //Запись последнего активного ключа из двери
+    // запрос ключа из двери 
+    // !!!!!!!!!!!!!!ШАБЛОН!!!!!!!!!!!!!!!
+    async getKeyFromDoor(req, res) {
+        const name = req.body.doorName
+        const door = await db.query(
+            'SELECT "lastKey" FROM doors WHERE name = $1',
+            [name]
+        )
+        ////////////////
+        // Здесь нужно организовать ПОСТ-запрос к двери
+        // const testRes = await axios.post('https://jsonplaceholder.typicode.com/posts', {
+        //     title: '222',
+        //     body: door.rows[0].name,
+        //     userID: door.rows[0].id,
+        // })
+        ////////////////
+        res.json(door.rows[0].lastKey)
+    }
+    // Запись последнего активного ключа из двери
     async writeLastKey(req, res) {
         const {id, lastKey} = req.body
 
-        console.log(id + lastKey)
-        res.json(id + lastKey)
+        // обновить последний ключ
+        //const keyResult = await db.query('UPDATE doors set "lastKey" = $1 WHERE id = $2 RETURNING *', [lastKey, id])
+        console.log(`${id} и ключ ${lastKey}`)
+        res.json(`${id} и ключ ${lastKey}`)
+        // res.json(keyResult.rows[0])
     }
 }
 
